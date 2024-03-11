bafec436cfd7650ac2f0871b4f42c53ee09f2e03
kimkyungsub <cyon256@icloud.com>
2024-03-11 12:13:00 +0900

    add auto-save plugin

--- lua/init.lua
+++ lua/init.lua
@@ -43,3 +43,19 @@ require("toggleterm").setup({
 	},
 })
 require("neoclipConfig")
+
+require("auto-save").setup({
+	trigger_events = { "InsertLeave" }, -- vim events that trigger auto-save. See :h events
+	-- function that determines whether to save the current buffer or not
+	-- return true: if buffer is ok to be saved
+	-- return false: if it's not ok to be saved
+	condition = function(buf)
+		local fn = vim.fn
+		local utils = require("auto-save.utils.data")
+
+		if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+			return true -- met condition(s), can save
+		end
+		return false -- can't save
+	end,
+})
