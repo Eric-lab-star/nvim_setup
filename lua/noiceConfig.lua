3cfebc8efe5ad5bb45483043ca3a488d41e95c75
kimkyungsub <cyon256@icloud.com>
2024-05-24 14:46:05 +0900

    noice

--- lua/noiceConfig.lua
+++ lua/noiceConfig.lua
@@ -1,7 +1,4 @@
 require("noice").setup()
-vim.keymap.set("n", "<leader>nl", function()
-	require("noice").cmd("last")
-end)
 
 vim.keymap.set("n", "<leader>nh", function()
 	require("noice").cmd("telescope")
