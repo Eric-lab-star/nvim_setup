94d171a84b83537d915067fae19d893f6c998fca
kimkyungsub <cyon256@icloud.com>
2024-10-13 08:57:05 +0900

    custom toggleterm

--- lua/plugins/toggleTerm.lua
+++ lua/plugins/toggleTerm.lua
@@ -25,6 +25,23 @@ return {
 			lazygit:toggle()
 		end
 
+		local compile = Terminal:new({
+			cmd = "cargo r",
+			display_name = "rust compiler",
+			on_open = function(term)
+				vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+			end,
+			direction = "float",
+			float_opts = {
+				border = "double",
+			},
+			close_on_exit = false,
+		})
+
 		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+		vim.keymap.set("n", "r", function()
+			compile:toggle()
+		end)
+
 	end,
 }
